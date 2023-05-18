81912d8e87a49327bd9eeb10a3684c4ae98c02c5
francesca2311 <f.tirino@studenti.unisa.it>
2023-05-18 21:31:47 +0100

    done angle, check distance

--- bot/reality.sh
+++ bot/reality.sh
@@ -1,3 +1,2 @@
 #! bin/bash
 gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=/home/francesca/Scrivania/MobileRobots_Project/bot/src/map2gazebo/map/map.yaml; exec bash'"
-sleep 10
