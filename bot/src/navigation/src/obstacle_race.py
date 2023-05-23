39ef5c84fcefba8891ed8645f7cd5b65601605b3
francesca2311 <f.tirino@studenti.unisa.it>
2023-05-23 12:31:30 +0100

    .

--- bot/src/navigation/src/obstacle_race.py
+++ bot/src/navigation/src/obstacle_race.py
@@ -7,7 +7,7 @@ from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 
 
-TIME_CHANGE = 3
+TIME_CHANGE = 0.5
 
 def spawn_udf_model(name,position):
     rospy.wait_for_service("/gazebo/spawn_urdf_model")
@@ -35,17 +35,21 @@ if __name__ == "__main__":
     rospy.wait_for_service('/gazebo/set_model_state')
     set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
-    points_list = [(5,0,0),(15.7,-5,0),(15.2,-2.5,0),(8.25,-9.5,0),(2.2,-10.9,0),(-4.75,-8.35,0),(24.5,-9.25,0),(36.5,-8.4,0),(32.4,-1.05,0),(21.85,-1.4,0)]
-    offesets = [(3,0.15,0),(0,3,0),(0,-6,0),(-4,0,0),(-1,1,0),(0,4,0),(-4,-1,0),(-1,4,0),(-4,0,0),(-1,1,0)]
+    points_list_x = [(15.2,-2.5,0),(-4.75,-8.35,0),(24.5,-9.25,0),(32.4,-1.05,0),(21.85,-1.4,0)]
+    points_list_z = [(5,0,0),(15.7,-5,0),(8.25,-9.5,0),(2.2,-10.9,0),(24.5,-9.25,0),(36.5,-8.4,0)]
+    
+    #offesets = [(3,0.15,0),(0,3,0),(0,-6,0),(-4,0,0),(-1,1,0),(0,4,0),(-4,-1,0),(-1,4,0),(-4,0,0),(-1,1,0)]
 
     #########OBSTACLES SPAWNING######################
-    for i in range(len(points_list)):
-        spawn_udf_model(name='object'+str(i),position=points_list[i])
+    for i in range(len(points_list_x)):
+        spawn_udf_model(name='object'+str(i),position=points_list_x[i])
+    for i in range(len(points_list_z)):
+        spawn_udf_model(name='object'+str(i+len(points_list_x)),position=points_list_z[i])
 
     rospy.sleep(TIME_CHANGE)
     #########POSITION CHANGING########################
     while not rospy.is_shutdown():
-        for i in range(len(offesets)):
+        for i in range(len(points_list_x)):
             x = points_list[i][0]+offesets[i][0]
             y = points_list[i][1]+offesets[i][1]
             z = points_list[i][2]+offesets[i][2]
