9a4a27d8f7af8cb7054b293baa7f42e2b8397a59
luigi <luigithebest936@gmail.com>
2023-05-23 11:26:40 +0200

    little objects and obstacles race improving

--- bot/src/navigation/src/obstacle_race.py
+++ bot/src/navigation/src/obstacle_race.py
@@ -6,6 +6,9 @@ from geometry_msgs.msg import Pose,Point
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
 
+
+TIME_CHANGE = 3
+
 def spawn_udf_model(name,position):
     rospy.wait_for_service("/gazebo/spawn_urdf_model")
     try:
@@ -21,27 +24,38 @@ def spawn_udf_model(name,position):
     except rospy.ServiceException as e:
         print("Service call failed: %s" % e)
 
+def change_state(name,new_state):
+    state = ModelState()
+    state.model_name = name
+    state.pose = Pose(position=Point(new_state[0],new_state[1],new_state[2]))
+    set_model_state(state)
+
 if __name__ == "__main__":
     rospy.init_node("spawn_udf_model_node")
     rospy.wait_for_service('/gazebo/set_model_state')
     set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
 
     points_list = [(5,0,0),(15.7,-5,0),(15.2,-2.5,0),(8.25,-9.5,0),(2.2,-10.9,0),(-4.75,-8.35,0),(24.5,-9.25,0),(36.5,-8.4,0),(32.4,-1.05,0),(21.85,-1.4,0)]
-
+    offesets = [(3,0.15,0),(0,3,0),(0,-6,0),(-4,0,0),(-1,1,0),(0,4,0),(-4,-1,0),(-1,4,0),(-4,0,0),(-1,1,0)]
 
     #########OBSTACLES SPAWNING######################
     for i in range(len(points_list)):
         spawn_udf_model(name='object'+str(i),position=points_list[i])
 
-    ##########OBJECT SPAWN#################
-    #spawn_udf_model()
-    #rospy.sleep(4)
-    """while rospy.is_shutdown:
-        ##########OBJECT POSITIONING#################
-        state = ModelState()
-        state.model_name = 'my_object'
-        state.pose = Pose(position=Point(5,0,0))
-        set_model_state(state)
-        rospy.loginfo('OBJECT MOVED.....')
-    rospy.spin()"""
-
+    rospy.sleep(TIME_CHANGE)
+    #########POSITION CHANGING########################
+    while not rospy.is_shutdown():
+        for i in range(len(offesets)):
+            x = points_list[i][0]+offesets[i][0]
+            y = points_list[i][1]+offesets[i][1]
+            z = points_list[i][2]+offesets[i][2]
+            change_state(name='object'+str(i),new_state=(x,y,z))
+    
+        rospy.sleep(TIME_CHANGE)
+        for i in range(len(offesets)):
+            x = points_list[i][0]
+            y = points_list[i][1]
+            z = points_list[i][2]
+            change_state(name='object'+str(i),new_state=(x,y,z))
+        rospy.sleep(TIME_CHANGE)
+    ##################################################
