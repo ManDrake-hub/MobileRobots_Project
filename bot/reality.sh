1cae9a4130a1ab815c433ca41a75d13b4553f4d8
francesca2311 <f.tirino@studenti.unisa.it>
2023-06-01 12:53:13 +0100

    test map

--- bot/reality.sh
+++ bot/reality.sh
@@ -1,5 +1,5 @@
 #! bin/bash
-gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=/home/francesca/Scrivania/MobileRobots_Project/bot/src/map2gazebo/map/map.yaml; exec bash'"
+gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch turtlebot3_navigation turtlebot3_navigation.launch map_file:=/home/francesca/Scrivania/MobileRobots_Project/bot/src/map2gazebo/map/map_new.yaml; exec bash'"
 sleep 3
 gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch navigation clear_costmap.launch; exec bash'"
 sleep 3
