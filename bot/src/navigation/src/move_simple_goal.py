49a637f21f30585b68a7008804b3b0faddb1792a
francesca2311 <f.tirino@studenti.unisa.it>
2023-06-07 11:58:00 +0100

    fix all; miss: optical flow, amcl refresh

--- bot/src/navigation/src/move_simple_goal.py
+++ bot/src/navigation/src/move_simple_goal.py
@@ -22,9 +22,15 @@ class Move:
         self.control_robot = RobotController(os.path.dirname(__file__) + "/waypoints.csv")
         self.calibration_service = rospy.ServiceProxy('calibration_server', Calibration)
         self.QR_service = rospy.ServiceProxy('QR_command',QR)
+        self.qr_sub = rospy.Subscriber('qr_data_topic', String, self.callback_qr)
         rospy.wait_for_service('calibration_server')
         self.pub_goal = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
         self.pub_rot = rospy.Publisher('/cmd_vel',Twist,queue_size=10)
+        self.listener = tf.TransformListener()
+        # Set initial robot position and orientation
+        self.robot_x = None
+        self.robot_y = None
+        self.robot_z = None
         self.rot_speed = 0.5
         self.update_step = 0.01
         self.actual_waypoint = None
@@ -32,7 +38,34 @@ class Move:
         self.command = None
         self.actual_goal = None
         rospy.sleep(3.0)
- 
+    
+    def clip_angle(self, angle):
+        distances = [abs(angle - 0), abs(angle - 90), abs(angle + 90), abs(angle - 180)]
+        min_distance = min(distances)
+        if min_distance == distances[0]:
+            return 0
+        elif min_distance == distances[1]:
+            return 90
+        elif min_distance == distances[2]:
+            return -90
+        else:
+            return 180
+        
+    # Get robot position 
+    def get_robot_position(self):
+        try:
+            (trans, rot) = self.listener.lookupTransform('/map', '/base_link', rospy.Time(0))
+            rot = euler_from_quaternion(rot)
+            self.robot_x = trans[0]
+            self.robot_y = trans[1]
+            self.robot_z = math.degrees(rot[2])
+            self.actual_waypoint = (self.robot_x,self.robot_y)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            pass
+
+    def callback_qr(self,msg):
+        self.set_fast()
+        
     # Calibrate robot 
     def calibration(self):
         # Calibration
@@ -54,40 +87,49 @@ class Move:
         rospy.sleep(3.0)
         #print(f"{self.msg.__str__()}")
     
-    # Set tolerance for send goal
-    def set_xy_goal_tolerance(self,value):
-        client = Client("move_base/DWAPlannerROS")
-        params = {'xy_goal_tolerance': value}
+    def set_amcl_params(self):
+        client = Client("amcl")
+        params = {'min_particles': 1000}
+        client.update_configuration(params)
+        params = {'force_update_after_initialpose': True}
+        client.update_configuration(params)
+        params = {'force_update_after_set_map': True}
+        client.update_configuration(params)
+        params = {'use_map_topic': True}
         client.update_configuration(params)
     
-    # Set tolerance for send goal
-    def set_yaw_goal_tolerance(self,value):
+    def set_move_base_params(self):
         client = Client("move_base/DWAPlannerROS")
-        params = {'yaw_goal_tolerance': value}
+        params = {'xy_goal_tolerance': 0.5}
         client.update_configuration(params)
-    
-    def set_transform_tolerance(self,value):
+        params = {'yaw_goal_tolerance': 3.14}
+        client.update_configuration(params)
         client = Client("move_base/global_costmap")
-        params = {'transform_tolerance': value}
+        params = {'transform_tolerance': 0.5}
+        client.update_configuration(params)
+        client = Client("move_base/global_costmap/inflation_layer")
+        params = {'inflation_radius': 0.5}
         client.update_configuration(params)
         client = Client("move_base/local_costmap")
-        params = {'transform_tolerance': value}
-        client.update_configuration(params)
-    
-    def set_min_particles(self,value):
-        client = Client("amcl")
-        params = {'min_particles': value}
-        client.update_configuration(params)
-
-    def set_inflation_radius(self,value):
-        client = Client("move_base/global_costmap/inflation_layer")
-        params = {'inflation_radius': value}
+        params = {'transform_tolerance': 0.5}
         client.update_configuration(params)
         client = Client("move_base/local_costmap/inflation_layer")
-        params = {'inflation_radius': value}
+        params = {'inflation_radius': 0.5}
         client.update_configuration(params)
 
+    def set_slow(self):
+        client = Client("move_base/DWAPlannerROS")
+        params = {'max_vel_x': 0.15, 'min_vel_x': -0.15, 'max_vel_trans':0.15, 'min_vel_trans': 0.08, 'max_vel_theta':1.0, 'min_vel_theta': 0.5, 'acc_lim_x':1.5, 'acc_lim_theta': 2.5}
+        client.update_configuration(params)
+
+    def set_fast(self):
+        client = Client("move_base/DWAPlannerROS")
+        params = {'max_vel_x': 0.26, 'min_vel_x': -0.26, 'max_vel_trans':0.26, 'min_vel_trans': 0.13, 'max_vel_theta':1.82, 'min_vel_theta': 0.9, 'acc_lim_x':2.5, 'acc_lim_theta': 3.2}
+        client.update_configuration(params)
+
+
     def rotate(self,angle):
+        #self.get_robot_position()
         move = Twist()
         move.linear.x = 0.0
         move.linear.y = 0.0
@@ -109,6 +151,7 @@ class Move:
         rospy.wait_for_message("move_base/result", MoveBaseActionResult)
         command = self.QR_service().answer 
         self.command = command.data 
+        #navigation.set_slow()
         #rospy.wait_for_message("move_base/result", MoveBaseActionResult)
         rospy.loginfo("Goal REACHED")
 
@@ -124,7 +167,7 @@ class Move:
         if self.command == "":
             rospy.loginfo("Startup the robot position then press ENTER")
             input()
-            self.calibration()
+            #self.calibration()
             self.pub_goal.publish(self.actual_goal)
         if self.command != 'stop' or self.command != "":
             self.next_waypoint, next_command,orientation,angle = self.control_robot.navigate(self.command, self.actual_waypoint)
@@ -143,17 +186,15 @@ class Move:
 
 if __name__ == "__main__":
     rospy.init_node("goal_custom")
-    #rospy.wait_for_service('QR_command') 
+    rospy.wait_for_service('QR_command') 
     state = None
     navigation = Move()
     rate = rospy.Rate(10.0)
-    navigation.set_xy_goal_tolerance(0.5)
-    navigation.set_yaw_goal_tolerance(3.14)
-    navigation.set_transform_tolerance(0.5)
-    navigation.set_min_particles(1000)
-    navigation.set_inflation_radius(0.5)
+    navigation.set_amcl_params()
+    navigation.set_move_base_params()
+    #navigation.set_slow()
     navigation.calibration()
-    print("END CALIBRATION")
+    #print("END CALIBRATION")
     navigation.move("straight on","real")
     while state != "FINISH":
         state = navigation.move()
