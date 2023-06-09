adbf52e37207b2c6662ce4cc7dfa6721ee49eb3e
giosullutrone <giovisull@gmail.com>
2023-06-09 09:31:48 +0200

    Modified code to use websockets

--- bot/src/camera/src/image_input_cv_left.py
+++ bot/src/camera/src/image_input_cv_left.py
@@ -7,44 +7,60 @@ from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image, CompressedImage
 from std_msgs.msg import String, Float32MultiArray, Int32MultiArray
 from optparse import OptionParser
+import asyncio
+import numpy as np
+import websockets
 
-def process_camera_lx_image(msg):
-    #rospy.sleep(0.5)
-    bridge = CvBridge()
-    img = bridge.compressed_imgmsg_to_cv2(msg)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    qr_decoder = cv2.QRCodeDetector()
+
+async def process_camera_image(websocket, path):
+    cv2.namedWindow('Camera lx', cv2.WINDOW_NORMAL)
+    cv2.namedWindow('Camera rx', cv2.WINDOW_NORMAL)
+
+    while not rospy.is_shutdown():
+        image_buffer = await websocket.recv()
+        nparr = np.frombuffer(image_buffer, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        camera_id = int(img[0, 0, 0])
+
+        # TODO: This has to be changed
+        if camera_id == 1:
+            cv2.imshow('Camera lx', img[1:])
+        elif camera_id == 2:
+            cv2.imshow('Camera rx', img[1:])
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        gray = gray[1:]
+
+        qr_decoder = cv2.QRCodeDetector()
         # decode QR code
-    try: 
-        decoded_text, points, _ = qr_decoder.detectAndDecode(gray)
-        if len(decoded_text)>0:
-            rospy.loginfo('LX QR code: %s', decoded_text)
+        try: 
+            decoded_text, points, _ = qr_decoder.detectAndDecode(gray)
+            if len(decoded_text)>0:
+                rospy.loginfo('%d QR code: %s', camera_id, decoded_text)
 
-            if len(points) > 0:
-                points = points[0].astype(int)
+                if len(points) > 0:
+                    points = points[0].astype(int)
 
-                cv2.polylines(img, [points], True, (0, 255, 0), 2)
-                cv2.putText(img, decoded_text, (points[0][0], points[0][1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-                pub.publish(decoded_text)
-    except CvBridgeError as e:
-            print(e)
-    except Exception as e:
-        pass
-    cv2.imshow('lx', img)
-    cv2.waitKey(1)
+                    cv2.polylines(img, [points], True, (0, 255, 0), 2)
+                    cv2.putText(img, decoded_text, (points[0][0], points[0][1] - 10),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                    pub.publish(decoded_text)
+        except CvBridgeError as e:
+                print(e)
+        except Exception as e:
+            pass
+        cv2.imshow('lx', img)
+        cv2.waitKey(1)
 
-def camera_image_lx_callback(image_msg):
-    process_camera_lx_image(image_msg)
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('image_input_lt', anonymous=True)
+        rospy.init_node('image_input', anonymous=True)
         pub = rospy.Publisher('qr_data_topic', String, queue_size=1)
         pub_params = rospy.Publisher("parameter_camera", Int32MultiArray, queue_size=1)
         # REALITY
-        sub_left = rospy.Subscriber('camera/lx/image', CompressedImage, camera_image_lx_callback)
-        rospy.sleep(0.5)
-        rospy.spin()
+        start_server = websockets.serve(process_camera_image, '192.168.178.65', 8000)
+        asyncio.get_event_loop().run_until_complete(start_server)
+        asyncio.get_event_loop().run_forever()
     except rospy.ROSInterruptException:
         pass
--- bot/src/realsense/src/realsense.py
+++ bot/src/realsense/src/realsense.py
@@ -6,31 +6,18 @@ import cv2 as cv
 import cv_bridge
 from sensor_msgs.msg import Image, CompressedImage
 from threading import Thread
+import asyncio
+import websockets
+import numpy as np
 
 class Node:
     def __init__(self) -> None:
         rospy.init_node(NODE_NAME, anonymous=True)
-        self.camera_id = rospy.get_param("~camera_id")
-        self.cap = self.open_stream()
-        self.set_camera()
-        self.print_camera_info()
-        self.bridge = cv_bridge.CvBridge()
-        self.img_pub = rospy.Publisher("image", CompressedImage, queue_size=30)
         self.rate = rospy.Rate(rospy.get_param("/camera/fps_publish"))
-        self.frame = None
-        Thread(target=self.publish_frame_cb, daemon=True).start()
-    
-    def open_stream(self):
-        '''
-        This code is opening a camera stream using OpenCV's `cv.VideoCapture()` method with the camera ID
-        specified in the `self.camera_id` variable. If the camera fails to open, it shuts down the ROS node. 
-        '''
-        cap = cv.VideoCapture(self.camera_id)
-        if not cap.isOpened():
-            rospy.logerr(f"Cannot open camera {self.camera_id}")
-            rospy.signal_shutdown("Camera error")
-        rospy.loginfo("Camera opened")
-        return cap
+
+        # TODO: This has to be changed
+        asyncio.get_event_loop().run_until_complete(self.publish_frame_cb(0))
+        asyncio.get_event_loop().run_until_complete(self.publish_frame_cb(1))
 
     def set_camera(self):
         '''
@@ -44,7 +31,7 @@ class Node:
         self.cap.set(cv.CAP_PROP_FRAME_WIDTH, rospy.get_param("/camera/width"))    
         self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, rospy.get_param("/camera/height"))   
 
-    def print_camera_info(self):
+    def print_camera_info(self, camera_id):
         '''
         These lines of code are retrieving the width, height, and frames per second (FPS) of the
         camera stream using OpenCV's `cv.VideoCapture()` method. The values are then logged using
@@ -53,19 +40,9 @@ class Node:
         width = self.cap.get(cv.CAP_PROP_FRAME_WIDTH)
         height = self.cap.get(cv.CAP_PROP_FRAME_HEIGHT)
         fps = self.cap.get(cv.CAP_PROP_FPS)
-        rospy.loginfo(f"Frame size width x height: {int(width)}x{int(height)}@{fps} for camera: {self.camera_id}")
+        rospy.loginfo(f"Frame size width x height: {int(width)}x{int(height)}@{fps} for camera: {camera_id}")
     
-    def publish_frame(self, frame):
-        '''
-        These lines of code are converting the OpenCV `frame` image to a compressed image message using the
-        `cv_bridge` library's `cv2_to_compressed_imgmsg()` method. The compressed image message is then
-        published to the ROS topic `"image"` using the `self.img_pub.publish()` method. This allows the
-        image to be transmitted over the ROS network with reduced bandwidth usage.
-        '''
-        img_msg = self.bridge.cv2_to_compressed_imgmsg(frame, 'jpg')
-        self.img_pub.publish(img_msg)
-    
-    def publish_frame_cb(self):
+    async def publish_frame_cb(self, camera_id):
         '''
         This code is running in a separate thread and continuously checking if the ROS node is still running
         using `rospy.is_shutdown()`. If the node is still running and there is a new frame available in
@@ -74,27 +51,24 @@ class Node:
         before checking again. This ensures that frames are continuously published to the ROS network at a
         consistent rate.
         '''
-        while not rospy.is_shutdown():
-            if self.frame is not None:
-                self.publish_frame(self.frame)
-            self.rate.sleep()
-        
-    def loop(self):
-        '''
-        This code is continuously reading frames from the camera stream using OpenCV's `cv.VideoCapture()`
-        method in a loop. It checks if the ROS node is still running using `rospy.is_shutdown()`. If the
-        node is still running and a new frame is successfully acquired, it sets the `self.frame` variable to
-        the new frame. If a frame is not acquired, it logs an error message using `rospy.logerr()`. This
-        loop ensures that frames are continuously read from the camera stream and stored in `self.frame` for
-        publishing to the ROS network.
-        '''
+        self.cap = cv.VideoCapture(camera_id)
+        self.set_camera()
+        self.print_camera_info()
+
+        ws = await websockets.connect('ws://192.168.178.65:8000')
+
         while not rospy.is_shutdown():
             ret, frame = self.cap.read()
-            if not ret:
-                rospy.logerr(f"Frame not acquired for camera {self.camera_id}")
-            self.frame = frame
+
+            if frame is None:
+                continue
+
+            frame_with_id = np.vstack((np.array([camera_id]), frame))  # Add camera ID to the image
+            _, image_buffer = cv.imencode('.jpg', frame_with_id)
+            await ws.send(image_buffer.tobytes())
+            self.rate.sleep()
+
     
 if __name__ == "__main__":
     NODE_NAME = "realsense_node"
     node = Node()
-    node.loop()
