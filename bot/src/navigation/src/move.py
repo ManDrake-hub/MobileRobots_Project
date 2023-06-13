591988d1911bd639e216a60604b23614fde70d19
francesca2311 <f.tirino@studenti.unisa.it>
2023-06-13 01:02:21 +0100

    FIX TRANSFORM

--- bot/src/navigation/src/move.py
+++ bot/src/navigation/src/move.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import rospy
 from move_base_msgs.msg import MoveBaseActionResult
 from geometry_msgs.msg import PoseStamped
@@ -11,6 +12,7 @@ from navigation.srv import Calibration
 from tf.transformations import euler_from_quaternion
 from robot_controller import RobotController
 from camera.srv import QR
+from dynamic_reconfigure.client import Client
 from robot_controller import RobotController
 from set_parameters import Parameters
 
@@ -30,6 +32,7 @@ class Move:
         rospy.wait_for_service('QR_command')
 
         # Subscribe to relevant topics and publish to others
+        self.qr_sub = rospy.Subscriber('qr_data_topic', String, self.callback_qr)
         self.sub_stop = rospy.Subscriber("stop", Bool, self.callback_recovery)
         self.pub_goal = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
         self.pub_rot = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
@@ -43,8 +46,15 @@ class Move:
         self.command = None
         self.actual_goal = None
         self.fake_goal = False
+        self.set_amcl_params()
+        self.set_move_base_params()
 
-    def robot_position_goal(self, msg):
+    def callback_qr(self, msg):
+        return
+        # Callback function for QR data
+        #self.set_fast()
+
+    def callback_recovery(self, msg):
         # Callback function for recovery
         self.real_robot.get_robot_position(False)
         if msg.data:
@@ -61,9 +71,6 @@ class Move:
             self.fake_goal = True
             self.pub_goal.publish(goal)
             return
-        return
-    
-    def fake_goal(self):
         self.pose_estimate.header.frame_id = "map"
         self.pose_estimate.pose.pose.position.x = self.real_robot.robot_x - 0.5
         self.pose_estimate.pose.pose.position.y = self.real_robot.robot_y - 0.5
@@ -83,21 +90,101 @@ class Move:
         rospy.sleep(0.5)
         self.fake_goal = False
 
-    def callback_recovery(self, msg):
-        self.robot_position_goal(msg)
-        self.fake_goal()        
-
     def calibration(self):
         # Calibrate the robot
         answer = self.calibration_service().answer
         return answer
 
-    def rotate_difference(self,angle,actual_orientation):
-        self.real_robot.get_robot_position(False)
-        real = self.real_robot.normalize(math.degrees(euler_from_quaternion(self.real_robot.robot_z)[2]))
-        final_rotate = self.real_robot.normalize((actual_orientation - real) + angle)
-        return final_rotate
-    
+    def set_amcl_params(self):
+        # Set parameters for amcl node
+        client = Client("amcl")
+        params = {
+            'min_particles': 1000,
+            'force_update_after_initialpose': True,
+            'force_update_after_set_map': True,
+            'update_min_d': 0.1,
+            'update_min_a': 0.1,
+            'gui_publish_rate': 10.0
+        }
+        client.update_configuration(params)
+
+    def set_move_base_params(self):
+        # Set parameters for move_base node
+        client = Client("move_base/DWAPlannerROS")
+        params = {'xy_goal_tolerance': 0.5, 'yaw_goal_tolerance': 3.14}
+        client.update_configuration(params)
+
+        client = Client("move_base/global_costmap")
+        params = {'transform_tolerance': 0.5}
+        client.update_configuration(params)
+        client = Client("move_base/global_costmap/inflation_layer")
+        params = {'inflation_radius': 1.0}
+        client.update_configuration(params)
+        client = Client("move_base/local_costmap")
+        params = {'transform_tolerance': 0.5}
+        client.update_configuration(params)
+        client = Client("move_base/local_costmap/inflation_layer")
+        params = {'inflation_radius': 0.7}
+        client.update_configuration(params)
+
+    def set_slow(self):
+        # Set slow movement parameters for the robot
+        client = Client("move_base/DWAPlannerROS")
+        params = {
+            'max_vel_x': 0.15,
+            'min_vel_x': -0.15,
+            'max_vel_trans': 0.15,
+            'min_vel_trans': 0.08,
+            'max_vel_theta': 1.0,
+            'min_vel_theta': 0.5,
+            'acc_lim_x': 1.5,
+            'acc_lim_theta': 2.5
+        }
+        client.update_configuration(params)
+
+    def set_medium(self):
+        # Set medium movement parameters for the robot
+        client = Client("move_base/DWAPlannerROS")
+        params = {
+            'max_vel_x': 0.2,
+            'min_vel_x': -0.2,
+            'max_vel_trans': 0.2,
+            'min_vel_trans': 0.14,
+            'max_vel_theta': 1.5,
+            'min_vel_theta': 0.75,
+            'acc_lim_x': 2.0,
+            'acc_lim_theta': 2.5
+        }
+        client.update_configuration(params)
+
+    def set_fast(self):
+        # Set fast movement parameters for the robot
+        client = Client("move_base/DWAPlannerROS")
+        params = {
+            'max_vel_x': 0.26,
+            'min_vel_x': -0.26,
+            'max_vel_trans': 0.26,
+            'min_vel_trans': 0.18,
+            'max_vel_theta': 1.82,
+            'min_vel_theta': 0.9,
+            'acc_lim_x': 2.5,
+            'acc_lim_theta': 3.2
+        }
+        client.update_configuration(params)
+
+    def send_goal(self, x, y, orientation):
+        # Send a goal position to the robot
+        self.msg.header.frame_id = "map"
+        self.msg.pose.position.x = float(x)
+        self.msg.pose.position.y = float(y)
+        self.msg.pose.orientation.x = orientation[0]
+        self.msg.pose.orientation.y = orientation[1]
+        self.msg.pose.orientation.z = orientation[2]
+        self.msg.pose.orientation.w = orientation[3]
+        self.pub_goal.publish(self.msg)
+        self.actual_goal = self.msg
+        rospy.sleep(3.0)
+
     def rotate(self, angle):
         # Rotate the robot by a specified angle
         move = Twist()
@@ -113,19 +200,12 @@ class Move:
             if abs(delta) >= abs(angle):
                 break
     
-    def send_goal(self, x, y, orientation):
-        # Send a goal position to the robot
-        self.msg.header.frame_id = "map"
-        self.msg.pose.position.x = float(x)
-        self.msg.pose.position.y = float(y)
-        self.msg.pose.orientation.x = orientation[0]
-        self.msg.pose.orientation.y = orientation[1]
-        self.msg.pose.orientation.z = orientation[2]
-        self.msg.pose.orientation.w = orientation[3]
-        self.pub_goal.publish(self.msg)
-        self.actual_goal = self.msg
-        rospy.sleep(3.0)
-    
+    def rotate_difference(self,angle,actual_orientation):
+        self.real_robot.get_robot_position(False)
+        real = self.real_robot.normalize(math.degrees(euler_from_quaternion(self.real_robot.robot_z)[2]))
+        final_rotate = self.real_robot.normalize((actual_orientation - real) + angle)
+        return final_rotate
+        
     def goal_reached(self, next_goal, orientation, angle, actual_orientation):
         _was_fake = False
         # Handle the situation when the goal is reached
@@ -134,13 +214,16 @@ class Move:
         self.send_goal(next_goal[0], next_goal[1], orientation)
         rospy.loginfo("Goal SEND")
         rospy.wait_for_message("move_base/result", MoveBaseActionResult)
+
         while self.fake_goal:
             _was_fake = True
             rospy.sleep(0.5)
+        
         if _was_fake:
             rospy.wait_for_message("move_base/result", MoveBaseActionResult)
         command = self.QR_service().answer
         self.command = command.data
+        #navigation.set_medium()
         rospy.loginfo("Goal REACHED")
 
     def move(self, command=None, real=None):
@@ -167,6 +250,7 @@ if __name__ == "__main__":
     rospy.init_node("move")
     state = None
     navigation = Move()
+    navigation.set_fast()
     navigation.move("straight on", "real")
     while state != "FINISH":
         state = navigation.move()
