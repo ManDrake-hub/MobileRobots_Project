ebceef95adfcbd244d5b2ed73afe1d32d438fbcb
francesca2311 <f.tirino@studenti.unisa.it>
2023-06-15 09:30:33 +0100

    fix launch

--- bot/simulation.sh
+++ bot/simulation.sh
@@ -3,8 +3,6 @@ gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roscore; exec bash'"
 sleep 3
 gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch turtlebot3_diem_sim turtlebot3_diem_sim.launch; exec bash'"
 sleep 3
-gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch navigation clear_costmap.launch; exec bash'"
-sleep 3
 gnome-terminal --tab -e "bash -c 'source devel/setup.bash; roslaunch navigation obstacle_race.launch; exec bash'"
 sleep 3
 gnome-terminal --tab -e "bash -c 'source devel/setup.bash; rosrun rqt_reconfigure rqt_reconfigure; exec bash'"
